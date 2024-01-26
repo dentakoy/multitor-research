911948ab5743476e9202175d3b87a8792f282013
Alexander Markin <sashamarkin@gmail.com>
2024-01-26 15:37:00 +0300

    Правки мультитора для тестов

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 readonly _dir="$(dirname "$(readlink -f "$0")")"
@@ -7,27 +8,27 @@ _arg="$1"
 
 if [[ "$1" == "install" ]] ; then
 
-  printf "%s\\n" "Create symbolic link to /usr/local/bin"
+  sudo printf "%s\\n" "Create symbolic link to /usr/local/bin"
 
   if [[ -e "${_dir}/bin/multitor" ]] ; then
 
     if [[ ! -e "/usr/local/bin/multitor" ]] ; then
 
-      ln -s "${_dir}/bin/multitor" /usr/local/bin
+      sudo ln -s "${_dir}/bin/multitor" /usr/local/bin
 
     fi
 
   fi
 
-  printf "%s\\n" "Create man page to /usr/local/man/man8"
+  sudo printf "%s\\n" "Create man page to /usr/local/man/man8"
 
   if [[ -e "${_dir}/static/man8/multitor.8" ]] ; then
 
     if [[ ! -e "/usr/local/man/man8/multitor.8.gz" ]] ; then
 
-      mkdir -p /usr/local/man/man8
-      cp "${_dir}/static/man8/multitor.8" /usr/local/man/man8
-      gzip /usr/local/man/man8/multitor.8
+      sudo mkdir -p /usr/local/man/man8
+      sudo cp "${_dir}/static/man8/multitor.8" /usr/local/man/man8
+      sudo gzip /usr/local/man/man8/multitor.8
 
     fi
 
@@ -35,19 +36,19 @@ if [[ "$1" == "install" ]] ; then
 
 elif [[ "$1" == "uninstall" ]] ; then
 
-  printf "%s\\n" "Remove symbolic link from /usr/local/bin"
+  sudo printf "%s\\n" "Remove symbolic link from /usr/local/bin"
 
   if [[ -L "/usr/local/bin/multitor" ]] ; then
 
-    unlink /usr/local/bin/multitor
+    sudo unlink /usr/local/bin/multitor
 
   fi
 
-  printf "%s\\n" "Remove man page from /usr/local/man/man8"
+  sudo printf "%s\\n" "Remove man page from /usr/local/man/man8"
 
   if [[ -e "/usr/local/man/man8/multitor.8.gz" ]] ; then
 
-    rm /usr/local/man/man8/multitor.8.gz
+    sudo rm /usr/local/man/man8/multitor.8.gz
 
   fi
 
@@ -58,3 +59,4 @@ else
 fi
 
 exit 0
+
